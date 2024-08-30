b6bde25d2e0377e24ea96934632343da6fd7f4a5
Luis <luisfilipebr123@gmail.com>
2024-08-30 09:29:45 -0300

    final

--- meu_blog/views.py
+++ meu_blog/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from .models import *
+
 # Create your views here.
 def index(request):
-    eulis = Eu.objects.all() # Select do django
+    eulis = Eu.objects.all() 
     informacoes = {
         'lista': eulis,
     }
     return render(request, 'index.html',informacoes)
 
 def sobre_me(request):
-    sobrelis = Sobre.objects.all() # Select do django
-    informacoes_sobre = {
-        'lista': sobrelis,
+    eulis = Eu.objects.all() 
+    informacoes = {
+        'lista': eulis,
     }
-    return render(request, 'sobre.html',informacoes_sobre)
+    return render(request, 'sobre.html',informacoes)
